dfd9db8997cec91653381cc9e87b1c613203c1ee
carlos-sweb <c4rl0sill3sc4@gmail.com>
2023-09-11 20:11:48 -0300

    improve regex from isUrl and isEmail

--- pp-is.js
+++ pp-is.js
@@ -1,7 +1,7 @@
 /*!!
  * Power Panel pp-is <https://github.com/carlos-sweb/pp-is>
  * @author Carlos Illesca
- * @version 1.0.8 (2022/09/11 22:26 PM)
+ * @version 1.0.9 (2022/09/11 23:09 PM)
  * Released under the MIT License
  */
 (function(global,factory){
@@ -12,42 +12,42 @@
       }()
   ));
   })(this,function(){
-      // =======================================================================      
-      getType = ( value) => Object.prototype.toString.call(value);
-      // =======================================================================
-      isArray = ( value ) => getType( value  ) === '[object Array]';
-      // =======================================================================
-      isRegExp = ( value ) => getType( value  ) === '[object RegExp]';
-      // =======================================================================
-      isBoolean = ( value ) => value === true || value === false || getType( value  ) === '[object Boolean]';
-      // =======================================================================
-      isDate = ( value ) => getType( value  ) === '[object Date]';
-      // =======================================================================
-      isElement = ( value ) => !!( value && value.nodeType === 1 );
-      // =======================================================================
-      isFunction = ( value ) => getType( value  ) === '[object Function]';
-      // =======================================================================
-      isNull = ( value ) => getType( value  ) === '[object Null]';
-      // =======================================================================
-      isNumber = ( value ) => getType( value  ) === '[object Number]';
-      // =======================================================================
-      isObject = ( value ) => getType( value  ) === '[object Object]';
-      // =======================================================================
-      isString = ( value ) => getType( value  ) === '[object String]';
-      // =======================================================================
-      isUndefined = ( value ) => getType( value  ) === '[object Undefined]';
       // ======================================================================= 
-      isEmpty = ( value ) => {
-          if(isString(value)){ return value === ""; }
+      function getType( value ){ return Object.prototype.toString.call(value) }
+      // =======================================================================      
+      function isArray ( value ){ return getType( value  ) === '[object Array]'}
+      // =======================================================================
+      function isRegExp ( value ){ return getType( value  ) === '[object RegExp]'}
+      // =======================================================================
+      function isBoolean( value ){ return value === true || value === false || getType( value  ) === '[object Boolean]'};
+      // =======================================================================
+      function isDate( value ){ return getType( value  ) === '[object Date]'}
+      // =======================================================================
+      function isElement( value ){ return !!( value && value.nodeType === 1 ) }
+      // =======================================================================
+      function isFunction( value ) { return getType( value  ) === '[object Function]'}
+      // =======================================================================
+      function isNull( value ){ return getType( value  ) === '[object Null]'}
+      // =======================================================================
+      function isNumber( value ){ return getType( value  ) === '[object Number]'}
+      // =======================================================================
+      function isObject( value ){ return getType( value  ) === '[object Object]'}
+      // =======================================================================
+      function isString( value ){ return getType( value  ) === '[object String]'}
+      // =======================================================================
+      function isUndefined( value ){ return getType( value  ) === '[object Undefined]'}
+      // ======================================================================= 
+      function isEmpty( value ){
+          if(isStringetType(value)){ return value === ""; }
           if(isArray(value) ){ return value.length == 0; }
           if(isObject(value) ){ return Object.keys(value).length === 0;}
           return true;
       }
       // =======================================================================
       // Investigar Sobre los dominios de cada pais y de cada segmento comercial o publico personal
-      isEmail = ( value ) => /^([a-z0-9_-]+[a-z0-9._-]+?)@([a-z0-9-]+\.[a-z]{2,6}|[a-z0-9]+\.[a-z]{2,6}\.[a-z]{2,6})$/i.test( value );
+      function isEmail( value ){ return /^([a-z0-9_-]+[a-z0-9._-]+?)@([a-z0-9-]+\.[a-z]{2,6}|[a-z0-9]+\.[a-z]{2,6}\.[a-z]{2,6})$/i.test( value )}
       // =======================================================================
-      isNaN = ( value ) => Number.isNaN( parseInt(value) );
+      function isNaN( value ){ return Number.isNaN( parseInt(value) )}
       // =======================================================================
       // ^(https?:\/\/)?([w]{3}\.|[w]{3}2\.)? protocol 
       //  ([w]{3}\.|[w]{3}2\.)? ([a-z\d]+\.)?([a-z\d]+\.[a-z]{2,} Domain name www or www2 
@@ -56,7 +56,7 @@
       //  (\:[\d]+)? -> port 
       // ([\??\/?]+[\/;&a-z\d%_.~+=-]*)? -> query url
       // (\#[\/;&a-z\d%_.~+=-]*)? -> hashtag url
-      isUrl = ( value ) => /^(https?:\/\/)?([w]{3}\.|[w]{3}2\.)?([a-z\d]+\.)?([a-z\d]+\.[a-z]{2,}|localhost|[\d]+\.[\d]+\.[\d]+\.[\d]+)(\:[\d]+)?([\??\/?]+[\/;&a-z\d%_.~+=-]*)?(\#[\/;&a-z\d%_.~+=-]*)?$/gi.test(value);          
+      function isUrl( value ){ return /^(https?:\/\/)?([w]{3}\.|[w]{3}2\.)?([a-z\d]+\.)?([a-z\d]+\.[a-z]{2,}|localhost|[\d]+\.[\d]+\.[\d]+\.[\d]+)(\:[\d]+)?([\??\/?]+[\/;&a-z\d%_.~+=-]*)?(\#[\/;&a-z\d%_.~+=-]*)?$/gi.test(value)}
       // =======================================================================
       var base = function( func ){
           // Retornamos una funcion
